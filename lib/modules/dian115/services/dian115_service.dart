4218c8e6d4a2a31b26c7fa33e46a8f858aaa170b
Via-berry <Via-berry@users.noreply.github.com>
2026-09-09 20:19:17 +0800

    fix(dian115): 补充 foundation 依赖导入，修复编译期 debugPrint 未定义错误

--- lib/modules/dian115/services/dian115_service.dart
+++ lib/modules/dian115/services/dian115_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:moviepilot_mobile/modules/dian115/models/dian115_models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
